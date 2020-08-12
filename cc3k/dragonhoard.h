298b283cba021d187c0fd801093cf8c3230c40f7
Kai Sun <bryant.kai1227@gmail.com>
2020-08-12 15:05:14 -0400

    add map

--- cc3k/dragonhoard.h
+++ cc3k/dragonhoard.h
@@ -9,3 +9,4 @@ class DragonHoard: public Treasure {
 };
 
 #endif
+
--- cc3k/main.cc
+++ cc3k/main.cc
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(){
-    
+    std::ifstream in{"defaultMap.txt"};
+    std::string map;
+    while(getline(in, map)){
+        std::cout << map << std::endl;
+    }
+
     return 0;
 }
+
